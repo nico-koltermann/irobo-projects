006a5f21ca694dd194e6b0b1da4a750a4fed73c7
nico-koltermann <nico.koltermann@get-racing.de>
2022-10-20 15:56:49 +0100

    Adjust script

--- src/evaluation/extract.sh
+++ src/evaluation/extract.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
-ROSBAG_NAME="vel_2022-10-19-14-48-37"
 YAML_PATH="./yaml"
 
 array=( "cmd_vel" )
 
-rosbag info -y "bags/$ROSBAG_NAME.bag" >> "$YAML_PATH/$ROSBAG_NAME-info.yaml"
+LAST_BAG=$(find bags/ -printf '%T+ %p\n' | sort -r | head -n1 | cut -d ' ' -f2 | cut -b 6-)
+
+if [[ $1 == "-h" ]]; then
+    echo "For the last bag, execute with argument -l"
+    echo "For the specific bag, execute with argument -f <bagname>"
+fi
+
+if [[ $1 == "-l" ]]; then
+    echo "Select Last Bag"
+    ROSBAG_NAME=$LAST_BAG
+fi
+
+if [[ $1 == "-f" ]]; then
+    echo "Select File"
+    if [ -f "bags/$2" ]; then
+        ROSBAG_NAME=$2
+    else 
+        echo "FILE NOT FOUND"
+    fi
+fi
+
+if [ -f "bags/$ROSBAG_NAME" ]; then
+    echo "FILE Ready"
+else 
+    echo "FILE NOT FOUND"
+    exit
+fi
+
+FILENAME="${ROSBAG_NAME%.*}"
+
+rosbag info -y "bags/$ROSBAG_NAME" >> "$YAML_PATH/$filename-info.yaml"
 
 for i in "${array[@]}"
 do
-    python3 ros_readbagfile.py  ./bags/$ROSBAG_NAME.bag "/$i" | tee $YAML_PATH/$ROSBAG_NAME-$i.yaml
+    python3 ros_readbagfile.py  ./bags/$ROSBAG_NAME "/$i" | tee $YAML_PATH/$FILENAME-$i.yaml
+    python3 scripts/plot_cmd.py -f $FILENAME-$i.yaml
 done
+
 
