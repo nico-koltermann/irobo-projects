6d36f5ad0cd7035fb7074e04a398cf5d1a47e8a5
PeterNoites <pndias10@gmail.com>
2022-11-03 09:17:19 -0700

    changed all

--- src/turtlebot3_markov_decision/src/turtlebot3_markov_decision.py
+++ src/turtlebot3_markov_decision/src/turtlebot3_markov_decision.py
@@ -22,6 +22,8 @@ class MarkovDecision():
         self.subscribe_to_topics()
         self.publish_to_topics()
 
+        self.goal = 2500
+
     def load_parameters(self):
         pass
 
@@ -61,27 +63,35 @@ class MarkovDecision():
         offset = -10
         cell_size = 0.3
 
-        self.cells = GridCells()
-        self.cells.header.stamp = rospy.Time().now()
-        self.cells.header.frame_id = "map"
-
-        self.cells.cell_height = cell_size
-        self.cells.cell_width = cell_size
+        sizex = range(0, msg.info.width)
+        sizey = range(0, msg.info.height)
+        self.array = np.zeros(x,y,5)
 
         i = 0
         for x in range(0, msg.info.width):
             for y in range(0, msg.info.height):
-                dataPoint = msg.data[y * msg.info.width + x] 
-                if dataPoint == 100 or dataPoint == 0:
+                dataPoint = msg.data[y * msg.info.width + x]
+
+                if dataPoint == 0 or dataPoint == 100:
+
                     marker = self.getEmptyMarker()
 
                     marker.id = i
                     i = i + 1
 
-                    # Set the color
-                    marker.color.r = 1.0 * random()
-                    marker.color.g = 1.0 * random()
-                    marker.color.b = 0.0
+                    if i == self.goal:
+                        marker.color.r = 1.0 
+                        marker.color.g = 1.0
+                        marker.color.b = 0.0
+                    elif dataPoint == 100:
+                        marker.color.r = 0.0
+                        marker.color.g = 0.0 
+                        marker.color.b = 0.0
+                    elif dataPoint == 0:
+                        marker.color.r = 0.6 * random()
+                        marker.color.g = 0.6 * random()
+                        marker.color.b = 0.0
+
                     marker.color.a = 1.0
 
                     marker.pose.position.x = (x * msg.info.resolution) + offset + (cell_size * 0.8)
@@ -90,15 +100,28 @@ class MarkovDecision():
 
                     self.finalArray.markers.append(marker)
 
-                    self.mapPoints.append( Point( 
-                        ((x * msg.info.resolution) + offset) + (cell_size * 0.8),
-                        ((y * msg.info.resolution) + offset) + (cell_size * 0.8),
-                        0
-                    ))
+                self.mapPoints.append( Point( 
+                    
+                    ((x * msg.info.resolution) + offset) + (cell_size * 0.8),
+                    ((y * msg.info.resolution) + offset) + (cell_size * 0.8),
+                    0
+                ))
+
+                self.array[x,y,1] = x
+                self.array[x,y,2] = y
+                self.array[x,y,3] = dataPoint
+                self.array[x,y,4] = ((x * msg.info.resolution) + offset) + (cell_size * 0.8)
+                self.array[x,y,5] = ((y * msg.info.resolution) + offset) + (cell_size * 0.8)
+
+                if i == self.goal:
+                    self.goalx = x
+                    self.goaly = y
 
         self.pub_viz.publish(self.finalArray)
 
         self.calculateMarkov()
+
+        np.savetxt('/home/irobo-projects/data.txt', np.around(self.array, decimals=2),fmt='%.2f',delimiter='\t')
 
 
     def calculateMarkov(self):
